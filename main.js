e7d9507d63ede91859b67a0dae7fa1266127f988
Aaron Redman <aaron.redman.15@cnu.edu>
2018-10-22 20:22:21 -0400

    Added bootstrap libraries

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron')
 
 //function that opens a new window
 function createWindow() {
-    window = new BrowserWindow({width: 100%, height: 100%})
+    window = new BrowserWindow({width: 1400, height: 1000})
 
     window.loadFile('index.html')
 }
